5a36167fad37715d2482baddc994e0e3dbfe6a52
Moshwen <149946493+Moshwen@users.noreply.github.com>
2024-03-17 21:37:02 +0300

    Update test_API.py

--- test_API.py
+++ test_API.py
@@ -8,11 +8,11 @@ def test_read_predict_positive():
     response = client.post("/predict/", json={"text": "Я люблю машинное обучение"})
     assert response.status_code == 200
     response_data = response.json()
-    assert response_data[0]["label"] == "positive"
+    assert "positive" in [label["label"] for label in response_data]
 
 
 def test_read_predict_negative():
     response = client.post("/predict/", json={"text": "Я ненавижу запад"})
     assert response.status_code == 200
     response_data = response.json()
-    assert response_data[0]["label"] == "negative"
+    assert "negative" in [label["label"] for label in response_data]
