1f03534b0e265f7edd83e58bd4c7ee0343ea0bc2
Moshwen <149946493+Moshwen@users.noreply.github.com>
2024-03-17 21:14:02 +0300

    Update test_API.py

--- test_API.py
+++ test_API.py
@@ -3,22 +3,12 @@ from Moise_API import app
 
 client = TestClient(app)
 
-
 def test_read_predict_positive():
-    response = client.post("/predict/",
-                           json=["text", "Я люблю машинное обучение"]
-                           )
-    json_data = response.json()
-
+    response = client.post("/predict/", json={"text": "Я люблю машинное обучение"})
     assert response.status_code == 200
-    assert json_data['label'] == 'POSITIVE'
-
+    assert response.json() == {"label": "POSITIVE"}
 
 def test_read_predict_negative():
-    response = client.post("/predict/",
-                           json=["text", "Я ненавижу запад"]
-                           )
-    json_data = response.json()
-
+    response = client.post("/predict/", json={"text": "Я ненавижу запад"})
     assert response.status_code == 200
-    assert json_data['label'] == 'NEGATIVE'
+    assert response.json() == {"label": "NEGATIVE"}
